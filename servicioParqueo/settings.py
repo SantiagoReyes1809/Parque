473fb00470de4bc9f8ec4fcd97d5ea0d073efd95
SantiagoReyes1809 <santiago.reyess@campusucc.edu.com>
2024-05-27 09:32:44 -0500

    cam

--- servicioParqueo/settings.py
+++ servicioParqueo/settings.py
@@ -1,23 +1,17 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6s6%d7qlzcl-ul30n_y^_410%%e98=b0013vdm4@*epv+922f4'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-
-# Application definition
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'parque.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['https://parque.azurewebsites.nett']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -60,10 +54,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'servicioParqueo.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -74,10 +64,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -94,27 +80,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
